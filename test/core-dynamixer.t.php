ed1cefb6a6ac3893c897fdef02311016727916cf
takashi <tatakatata@gmail.com>
2009-03-28 00:43:12 +0900

    set test number

--- test/core-dynamixer.t.php
+++ test/core-dynamixer.t.php
@@ -4,29 +4,29 @@ require_once('lib/lime.php');
 require_once('lib/kaden-carp.php');
 require_once('lib/kaden-core-dynamixer.php');
 
-$t = new lime_test(3
+$t = new lime_test(9
                    , new lime_output_color());
 
 $t->ok( $dm = new Kaden_Core_Dynamixer(new DummyCx), 'constructer' );
 
-$nanashi = new Person($t, $dm);
-$t->is_deeply( $nanashi, $dm->get_instance('nanashi'), 'get_instance()' );
+$takashi = new Person($t, $dm, 'takashi');
+$t->is_deeply( $takashi, $dm->get_instance('takashi'), 'get_instance()' );
 
 ob_start();
 $dm->call_assigned_method('profile');
 $contents = ob_get_contents();
 ob_end_clean();
-$t->is_deeply($contents, "I am nanashi.\n", 'call_assigned_method()');
+$t->is_deeply($contents, "I am takashi.\n", 'call_assigned_method()');
 
 $t->ok( $dm->set_instance(new Dog, 'dog'), 'set_instance() from outer' );
 $t->ok( $dog = $dm->get_instance('dog'), 'get_instance()' );
-$t->ok( $dm->assign_method(array($dog, 'profile'), 'profile'), 'assigned_method() from outer, first argument is array');
+$t->ok( $dm->assign_method(array($dog, 'bark'), 'profile'), 'assigned_method() from outer, first argument is array');
 
 ob_start();
 $dm->call_assigned_method('profile');
 $contents = ob_get_contents();
 ob_end_clean();
-$t->is_deeply($contents, "I am nanashi.\n" . "Bow! Bow!\n", 'call_assigned_method()');
+$t->is_deeply($contents, "I am takashi.\n" . "Bow! Bow!\n", 'call_assigned_method()');
 
 
 class DummyCx{};
@@ -48,7 +48,7 @@ class Person
 
 class Dog
 {
-    function profile(){
+    function bark(){
         echo "Bow! Bow!\n";
     }
 }
